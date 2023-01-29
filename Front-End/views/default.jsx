b452c9fddb4eb8c1480a88e773d3b8904c11278c
Your Name <you@example.com>
2023-01-29 13:41:06 -0800

    Added css and updated default

--- Front-End/views/default.jsx
+++ Front-End/views/default.jsx
@@ -5,7 +5,7 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-                <link rel="stylesheet" href="/style.css"/>
+                <link rel="stylesheet" href="/css/style.css"/>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"/>
             </head>
             <body>
