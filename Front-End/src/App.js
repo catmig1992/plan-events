34d7e61081b57f57538ea2c330299cc8428ad9c3
Your Name <you@example.com>
2023-01-29 14:17:24 -0800

    Added new code in app.js

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -5,18 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 className="title">SPARKLY EVENT PLANNER</h1>
+    <div>
+      <img className="main-image"  src="/image/event_table.jpeg" alt="balloons"/>
+      <div>
+       <h2 className="About">About us</h2>
+   
+        <p className="About">Planning a PARTY is our business! We are here for your every need to get the party started.</p>
+        <p className="About">We have been planning a party since 2010 and have been partying for 13 years!</p>
+        <p className="About">To get started, click "Build Your Own Event" and fill out the form. If you have questions.</p>
+      </div>
+      </div>
       </header>
     </div>
   );
