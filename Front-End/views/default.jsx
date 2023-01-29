f1473b24135c8cb0a7bce9747a6c2f1e92e1fef6
Your Name <you@example.com>
2023-01-29 13:28:49 -0800

    Added Views folder with default, gallery, home

---
+++ Front-End/views/default.jsx
@@ -0,0 +1,34 @@
+const React = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+                <link rel="stylesheet" href="/style.css"/>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"/>
+            </head>
+            <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a className="Nav-text" href="/">Home</a>
+                        </li>
+                        <li>
+                        <a className="Nav-text" href="/services">Services</a>
+                        </li>
+                        <li>
+                        <a className="Nav-text"href="/gallery">Gallery</a>
+                        </li>
+                        <li><a href="">
+        <button className="button">Build Your Own Event</button>
+    </a></li>
+                    </ul>
+                    </nav>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = Def
---
+++ Front-End/views/home.jsx
@@ -0,0 +1,29 @@
+const React = require('react')
+const Def = require('./default.jsx')
+
+function home () {
+    return (
+      <Def>
+<main>
+  
+    <h1 className="title">SPARKLY EVENT PLANNER</h1>
+    <div>
+      <img className="main-image"  src="/image/event_table.jpeg" alt="balloons"/>
+      <div>
+       <h2 className="About">About us</h2>
+   
+        <p className="About">Planning a PARTY is our business! We are here for your every need to get the party started.</p>
+        <p className="About">We have been planning a party since 2010 and have been partying for 13 years!</p>
+        <p className="About">To get started, click "Build Your Own Event" and fill out the form. If you have questions.</p>
+      </div>
+    </div>
+    
+</main>
+
+
+      </Def>
+    )
+  }
+  
+
+module.exports = home
