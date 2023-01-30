743f06642b8ff1da5708504294a74f2fd5aa48a5
Your Name <you@example.com>
2023-01-29 19:07:08 -0800

    Adding codes to css and fixing app.js. Tried fixing gallery.js in components, but it is not showing anything.I will continue to work on it.

---
+++ Front-End/src/components/gallery.js
@@ -0,0 +1,30 @@
+
+export default function Gallery() {
+  return (
+    /////Navigation Bar + Homepage Information///////
+    <div className="App">
+       <nav>
+        <ul>
+          <li>
+            <a className="Nav-text" href="/">Home</a>
+            </li>
+            <li>
+              <a className="Nav-text" href="/services">Services</a>
+              </li>
+              <li>
+                <a className="Nav-text"href="/gallery">Gallery</a>
+                </li>
+                <li><a href="">
+        <button className="button">Build Your Own Event</button>
+    </a>
+    </li>
+    </ul>
+    </nav>
+
+
+</div>
+
+
+  );
+}
+
