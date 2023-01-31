5a9dc9e7b3077fe2a555a116c9ee14f98f4c1a8d
Your Name <you@example.com>
2023-01-31 06:27:46 -0800

    Made code changes to gallery, navbar, and app.js

--- Front-End/src/components/NavBar.js
+++ Front-End/src/components/NavBar.js
@@ -2,17 +2,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from "react";
 import "../App.css";
 import Gallery from "./Gallery";
+import About from "./About";
 
 export default function NavBar() {
   return (
     /////Navigation Bar///////
     <div className="App">
-      <Router>
         <nav>
           <ul>
             <li>
               <a className="Nav-text" href="/">
                 Home
+              </a>
+            </li>
+            <li>
+              <a className="Nav-text" href="/About">
+                About
               </a>
             </li>
             <li>
@@ -32,13 +37,15 @@ export default function NavBar() {
             </li>
           </ul>
         </nav>
-
+<Router>
         <div className="display">
           {/* <Route path="/" component={Home} /> */}
           {/* <Route path="/services" component={Services} /> */}
-          <Route path="/packages" render={() => <Gallery />} />
+          <Route path="/gallery" render={() => <Gallery />} />
+          <Route path="/about" render={() => <About />} />
         </div>
       </Router>
     </div>
   );
 }
+
--- Front-End/src/components/gallery.js
+++ Front-End/src/components/gallery.js
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function Gallery() {
   return (
-    <div className="App">
-      <h1>Gallery</h1>
-    </div>
+    <div class="ImageGallery">
+<div id="myDIV">
+  <div class="item1"><img className="image" src="/image/wedding.jpeg" alt="wedding"/></div>
+  <div class="item2"><img className="image" src="/image/party_people.jpeg" alt="party people"/></div>
+  <div class="item3"><img className="image" src="/image/party_people2.jpeg" alt="party people 2"/></div>
+  </div>
+
+  <div id="myDIV">
+  <div class="item4"><img className="image" src="/image/newyear_celebration.jpeg" alt="new year celebration"/></div>
+  <div class="item5"><img className="image" src="/image/corporate_party.jpeg" alt="corporate party"/></div>
+  <div class="item6"><img className="image" src="/image/birthday.jpeg" alt="birthday"/></div>
+</div>
+</div>
+
   );
 }
