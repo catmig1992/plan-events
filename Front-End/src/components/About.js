39a5537c822ea7728fc392c559c669eb745734b8
Your Name <you@example.com>
2023-02-03 22:36:22 -0800

    Added codes to App.css, About.js, and added readme content

--- Front-End/src/components/About.js
+++ Front-End/src/components/About.js
@@ -3,23 +3,20 @@ import React from "react";
 export default function Home() {
   return (
     <div className="About">
-      <h2 className="About_2">About us</h2>
-      <div>
-        <img
-          className="main-image"
-          src="/image/event_table.jpeg"
-          alt="balloons"
-        />
-        <div className="background_About">
-
-          <p className="About_text">
+      <div class="about_row">
+  <div class="about_column">
+    <img src="/image/event_table.jpeg" alt="Event-Table" width="900px" height="400px" className="About_image"/>
+  </div>
+  <div class="column">
+  <h2 className="About_Title">About us</h2>
+  <p className="About_text">
             Planning a PARTY is our business! We are here for your every need to
             get the party started. We have been planning a party since 2010 and have been partying for
             13 years! To get started, click "Build Your Own Event" and fill out the form.
             If you have questions.
           </p>
-        </div>
-    </div>
+  </div>
+</div>
     </div>
   );
 }
